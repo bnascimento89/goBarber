6ec48ab0912cf158459fe4bde32317186ff53ef9
bruno.nascimento <bruno.nascimento@xpi.com.br>
2021-02-20 18:24:37 -0300

    type orm model

--- PrimeiroProjetoNode/src/models/Appointment.ts
+++ PrimeiroProjetoNode/src/models/Appointment.ts
@@ -1,17 +1,24 @@
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     provider: string;
 
+    @Column('timestamp with time zone')
     date: Date;
 
-    constructor({ provider, date }: Omit<Appointment, 'id'>) {
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
+    // Código sem BD TypeORM
+    // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    //     this.id = uuid();
+    //     this.provider = provider;
+    //     this.date = date;
+    // }
 }
 
 export default Appointment;
