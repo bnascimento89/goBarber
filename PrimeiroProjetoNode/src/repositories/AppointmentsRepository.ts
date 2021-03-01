c6cdb2ffda15edb87dffc92dedd1f974d432d396
bruno.nascimento <bruno.nascimento@xpi.com.br>
2021-03-01 09:03:25 -0300

    repository

--- PrimeiroProjetoNode/src/repositories/AppointmentsRepository.ts
+++ PrimeiroProjetoNode/src/repositories/AppointmentsRepository.ts
@@ -1,37 +1,48 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-    provider: string;
-    date: Date;
-}
+// Aula Anterior: Utilizar a parte de criação do TypeORM
 
-class AppointmentsRepository {
-    private appointments: Appointment[];
+// interface CreateAppointmentDTO {
+//     provider: string;
+//     date: Date;
+// }
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+    // #Aula Anterior
+    // TypeORM já foi método de buscar todos e não iremos mais salvar em []
+    // private appointments: Appointment[];
 
-    constructor() {
-        this.appointments = [];
-    }
+    // constructor() {
+    //     this.appointments = [];
+    // }
 
-    public all(): Appointment[] {
-        return this.appointments;
-    }
+    // public all(): Appointment[] {
+    //     return this.appointments;
+    // }
 
-    public findByDate(date: Date): Appointment | null {
-        const findAppointment = this.appointments.find(appointment =>
-            isEqual(date, appointment.date),
-        );
+    public async findByDate(date: Date): Promise<Appointment | null> {
+        // const findAppointment = this.appointments.find(appointment =>
+        //     isEqual(date, appointment.date),
+        // );
+
+        const findAppointment = await this.findOne({
+            where: { date },
+        });
 
         return findAppointment || null;
     }
 
-    public create({ provider, date }: CreateAppointmentDTO): Appointment {
-        const appointment = new Appointment({ provider, date });
+    // #Aula Anterior
+    // TypeORM já possuí um metódo de create
+    // public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    //     const appointment = new Appointment({ provider, date });
 
-        this.appointments.push(appointment);
+    //     this.appointments.push(appointment);
 
-        return appointment;
-    }
+    //     return appointment;
+    // }
 }
 
 export default AppointmentsRepository;
